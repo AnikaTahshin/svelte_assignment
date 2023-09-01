026864918a617b4b7ab21077b05065c3f8377f90
unknown <anikatahsinmeem@gmail.com>
2023-09-01 23:25:16 +0600

    route and layout page on processing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+  import "../app.css";
+</script>
+
+<slot />
+
+<main class="grid grid-cols-2 box-border h-screen">
+  <div class="grid grid-rows-2 box-border">
+    <div class="grid grid-cols-2">
+      <div class="grid grid-rows-2">
+        <div class="bg-blue-400 box-border" />
+        <div class="bg-teal-400 box-border" />
+      </div>
+      <div class="bg-rose-100 box-border" />
+    </div>
+    <div class="bg-fuchsia-400 box-border" />
+  </div>
+
+  <div class="grid grid-rows-2 box-border">
+    <div class="bg-blue-400 box-border" />
+    <div class="grid grid-cols-2 box-border">
+      <div class="bg-yellow-100 box-border">
+        <div class="grid grid-cols-2 place-items-center">
+          <div class="bg-orange-500 box-border"><p>ani</p></div>
+        </div>
+      </div>
+      <div class="bg-emerald-400 box-border" />
+    </div>
+  </div>
+</main>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { onMount } from "svelte";
+  import Map from "ol/Map.js";
+  import OSM from "ol/source/OSM.js";
+  import TileLayer from "ol/layer/Tile.js";
+  import View from "ol/View.js";
+
+  let map;
+
+  onMount(() => {
+    map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+  });
+</script>
+
+layer.setStyle(color: "#006a4e", opacity: 0.75,)
+<div id="map" />
+
+<style lang="postcss">
+  :global(body) {
+    margin: 0;
+    height: 100%;
+  }
+
+  #map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
