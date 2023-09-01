27d89f5a91e301f5b2347f8c3cb39a4d91ab8448
unknown <anikatahsinmeem@gmail.com>
2023-09-02 00:14:12 +0600

    mountain sticker done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <slot />
 
-<main class="grid grid-cols-2 box-border h-screen">
+<main class="grid lg:grid-cols-2 sm:grid-cols-1 box-border h-screen">
   <div class="grid grid-rows-2 box-border">
     <div class="grid grid-cols-2">
       <div class="grid grid-rows-2">
@@ -19,9 +19,19 @@
   <div class="grid grid-rows-2 box-border">
     <div class="bg-blue-400 box-border" />
     <div class="grid grid-cols-2 box-border">
-      <div class="bg-yellow-100 box-border">
-        <div class="grid grid-cols-2 place-items-center">
-          <div class="bg-orange-500 box-border"><p>ani</p></div>
+      <div class="flex justify-center items-center bg-yellow-100 box-border">
+        <div class=" flex justify-center items-center">
+          <div
+            class="bg-[#293462] box-border h-[300px] w-[300px] flex justify-center items-center"
+          >
+            <div
+              id="circle"
+              class="bg-yellow-100 overflow-hidden relative h-[200px] w-[200px] rounded-full flex justify-center items-center"
+            >
+              <div class="right-tri" />
+              <div class="left-tri" />
+            </div>
+          </div>
         </div>
       </div>
       <div class="bg-emerald-400 box-border" />
@@ -32,5 +42,26 @@
 <style lang="postcss">
   :global(html) {
     background-color: theme(colors.gray.100);
+  }
+
+  .right-tri {
+    width: 0;
+    height: 0;
+    border-bottom: 200px solid #fe5f55;
+    border-right: 200px solid transparent;
+    border-left: 200px solid transparent;
+    position: absolute;
+    top: 60px;
+    left: -59px;
+  }
+  .left-tri {
+    width: 0;
+    height: 0;
+    border-bottom: 100px solid #fe5f55;
+    border-right: 100px solid transparent;
+    border-left: 100px solid transparent;
+    position: absolute;
+    top: 130px;
+    left: -70px;
   }
 </style>
